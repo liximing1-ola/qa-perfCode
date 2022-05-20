78eaf683330a6dc447bfd8c05eff2ba459f82350
liximing1013 <973878442@qq.com>
2022-05-20 14:47:11 +0800

    ‘提交修改’

--- mobilePerf/run.sh
+++ mobilePerf/run.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+basedir=`cd $(dirname $0); pwd -P`
+echo $basedir
+echo 11111
 python ~/PycharmProjects/perfCode/mobilePerf/tools/changeFile.py
 python ~/PycharmProjects/perfCode/mobilePerf/tools/csvToChart.py cpu
 python ~/PycharmProjects/perfCode/mobilePerf/tools/csvToChart.py fps
