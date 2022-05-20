d03ca6f0f7838a02eef666e178edf358b38c0d0a
li.ximing <973878442@qq.com>
2022-05-20 12:15:02 +0800

    'change'

--- mobilePerf/run.sh
+++ mobilePerf/run.sh
@@ -1,3 +1,4 @@
+python ~/PycharmProjects/perfCode/mobilePerf/tools/changeFile.py
 python ~/PycharmProjects/perfCode/mobilePerf/tools/csvToChart.py cpu
 python ~/PycharmProjects/perfCode/mobilePerf/tools/csvToChart.py fps
 python ~/PycharmProjects/perfCode/mobilePerf/tools/csvToChart.py mem
