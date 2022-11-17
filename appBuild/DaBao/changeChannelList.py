fdc4eefcda048143591d0058bfb119051779d49b
li.ximing <li.ximing@olaola.chat>
2022-11-17 18:37:01 +0800

    '代码优化'

--- appBuild/DaBao/changeChannelList.py
+++ appBuild/DaBao/changeChannelList.py
@@ -11,11 +11,12 @@ app_name = sys.argv[2]
 app_version = sys.argv[3]
 app_so = sys.argv[4]
 
+
 # 出主渠道包
 def process_one_conf(channel_list):
     now = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     # 本地路径
-    output_path ='E:/Build/{}_{}_{}_{}'.format(app_name, app_version, app_so, now)
+    output_path = 'D:/Build/{}_{}_{}_{}'.format(app_name, app_version, app_so, now)
     if os.path.exists(output_path):
         print('existed')
     else:
@@ -28,18 +29,20 @@ def process_one_conf(channel_list):
     print(app_list)
     print('--------------BuildSuccess，ApkPath==[ {} ]--------------'.format(output_path))
 
+
 def change_name(path):
     os.chdir(path)
     app_list = os.listdir(path)
     for app in app_list:
         if app.endswith('.apk'):
-            apk_name=app[:-4]
-            apk_name_1=apk_name.split('-')
-            channel_name=apk_name_1[-1].split('_')[1]
-            app_name_2=apk_name_1[0] + '-' + channel_name + '-release' + '-{}'.format(app_so)
+            apk_name = app[:-4]
+            apk_name_1 = apk_name.split('-')
+            channel_name = apk_name_1[-1].split('_')[1]
+            app_name_2 = apk_name_1[0] + '-' + channel_name + '-release' + '-{}'.format(app_so)
             os.rename(apk_name + '.apk', app_name_2 + '.apk')
         else:
             print('Not OK')
+
 
 def appBuild():
     if app_name.startswith('banban'):
