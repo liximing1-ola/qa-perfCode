b334708b3f4b5c684ecc00b9bacfd6b1363b1365
li.ximing <li.ximing@olaola.chat>
2022-12-06 11:42:04 +0800

    '代码优化'

---
+++ appBuild/changeImage.py
@@ -0,0 +1,32 @@
+import sys
+from PIL import Image
+import os
+
+
+#  图片置灰
+#  https://blog.csdn.net/wzk4869/article/details/126082728
+def imageChange():
+    if len(sys.argv) != 3:
+        print('error')
+        exit(1)
+    image_path = sys.argv[1]
+    out_path = sys.argv[2]
+    # image_path = 'C:\Users\banban\Desktop\image'
+    # out_path = 'C:/Users/banban\Desktop/new_image'
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+
+    file_list = os.listdir(image_path)
+    print(len(file_list))
+    for file in file_list:
+        if file.endswith('.png') or file.endswith('.jpg'):
+            image_i = Image.open(os.path.join(image_path, file))
+            image_gray = image_i.convert('L')
+            print('{}-gray success'.format(file))
+            image_gray.save(os.path.join(out_path, file), quality=95)  # subsampling=0
+
+    print(len(os.listdir(out_path)))
+
+
+if __name__ == '__main__':
+    imageChange()
